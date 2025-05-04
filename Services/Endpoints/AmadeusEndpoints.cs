403eecd8d6cbd1f249aae09b58938ad9fc232371
ElanStintz <elans366@gmail.com>
2025-05-04 20:14:36 +0100

    fixed parent/sub folder structure

--- Services/Endpoints/AmadeusEndpoints.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RouteWise.Services.Endpoints
-{
-    public static class AmadeusEndpoints
-    {
-        public const string AuthenticationEndpoint = "https://test.api.amadeus.com/v1/security/oauth2/token";
-        public const string FlightDestinationsEndpoint = "https://test.api.amadeus.com/v1/shopping/flight-destinations";
-        public const string FlightOffersEndpoint = "https://test.api.amadeus.com/v2/shopping/flight-offers";
-    }
-}
